dd970c3e05ecf31155e6383e6ec1af91e2a2d272
Aleksandr Grankin <alleksgran@gmail.com>
2025-03-12 19:07:13 +0700

    Поработал над адаптивностью

--- src/components/CardSection/CardSection.jsx
+++ src/components/CardSection/CardSection.jsx
@@ -7,17 +7,17 @@ const CardSection = () => {
     return (
         <div className="card__section">
             <InteractiveBorderCard
-                outerSize={25}
+                outerSize={28}
                 backColorStartLiner={'#9AC9FF'}
                 backColorEndLiner={'#4387D6'}
             ></InteractiveBorderCard>
             <InteractiveBorderCard
-                outerSize={25}
+                outerSize={28}
                 backColorStartLiner={'#EA8E8E'}
                 backColorEndLiner={'#D64636'}
             ></InteractiveBorderCard>
             <InteractiveBorderCard
-                outerSize={25}
+                outerSize={28}
                 backColorStartLiner={'#EAC17E'}
                 backColorEndLiner={'#D18B17'}></InteractiveBorderCard>
         </div>
