59ea5d1cf66826f6524ae00aa941a605971a40bf
Aleksandr Grankin <alleksgran@gmail.com>
2025-03-12 19:51:50 +0700

    Поработал над адаптивностью

--- src/components/InteractiveBorderCard/InteractiveBorderCard.jsx
+++ src/components/InteractiveBorderCard/InteractiveBorderCard.jsx
@@ -6,14 +6,10 @@ const InteractiveBorderCard = ({outerSize, backColorStartLiner, backColorEndLine
     return (
         <div className="interactive__border__card">
             <div style={{
-                height: outerSize * 1.6 + 'vw',
-                width: outerSize + 'vw',
             }}
                  className='interactive__border__card__outer'
             >
                 <div style={{
-                    height: (outerSize * 1.6 - (outerSize * 1.6 * 0.09)) + 'vw',
-                    width: (outerSize - (outerSize * 0.14)) + 'vw',
                     background: `linear-gradient(to bottom, ${backColorStartLiner}, ${backColorEndLiner})` // Линейный градиент от #0A4589 до #062A55
                 }}
                      className='interactive__border__card__inner'
